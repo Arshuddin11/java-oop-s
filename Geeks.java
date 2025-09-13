578322d2fee1b449d843746d2296f27091f1ee9a
Arshuddin11 <arsh.uddin.cs28@iilm.edu>
2025-09-13 11:48:15 +0530

    Create Geeks.java

---
+++ Geeks.java
@@ -0,0 +1,13 @@
+class Geeks {
+    Geeks() {
+        super();
+        System.out.println("Constructor Called");
+        System.out.println("My name is Divye");
+    }
+
+    public static void main(String[] args) {
+        Geeks geek = new Geeks();
+    }
+}
+
+
